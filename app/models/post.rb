391144088f20bfdb918b9da5c0ad88261c6b2bc3
sethstrouf <seth.strouf@doane.edu>
2023-06-16 15:36:52 -0500

    Destroy comments when destroying Post

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,5 +1,5 @@
 class Post < ApplicationRecord
-  has_many :comments, -> { order(created_at: :desc) }
+  has_many :comments, -> { order(created_at: :desc) }, dependent: :destroy
 
   has_many_attached :images
 
